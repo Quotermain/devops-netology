92370d909a975c652128195d0fdbb72ca6e4e9c1
Павел Хат <pavelhat@gmail.com>
2022-06-26 22:52:09 +0500

    All is done.

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
-Вывод ```terraform apply```:   
-![](screenshots/terrafrom_apply.png)   
-
-Ссылка на [файл](main.tf).
-
-Образ можно создать с помощью Packer.
+Сделал то, чему нашёл аналогию на Yandex Cloud.   
+Вывод команды terraform ```workspace list```:    
+![](screenshots/terraform_workspace_list.png)   
+Фрагмент вывода команды ```terraform plan``` для воркспейса ```prod```:   
+![](screenshots/terraform_plan.png)
--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-terraform {
-  required_providers {
-    yandex = {
-      source = "yandex-cloud/yandex"
-    }
-  }
-  required_version = ">= 0.13"
-}
-
 provider "yandex" {
   token     = var.YC_TOKEN
   cloud_id  = "b1gpf0sdmtpvil39scs3"
@@ -14,11 +5,54 @@ provider "yandex" {
   zone      = "ru-central1-b"
 }
 
-resource "yandex_compute_image" "foo-image" {
-  name       = "my-custom-image"
-  source_image = "fd8hafusaj96qep5ub6h"
+resource "yandex_compute_instance" "default" {
+  name        = "test"
+  platform_id = "standard-v1"
+  zone        = "ru-central1-a"
+
+  count = local.instance_count_map[terraform.workspace]
+
+  resources {
+    cores  = 2
+    memory = 4
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8hafusaj96qep5ub6h"
+    }
+  }
+
+  network_interface {
+    subnet_id = "${yandex_vpc_subnet.foo.id}"
+  }
+
+  metadata = {
+    foo      = "bar"
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+}
+
+resource "yandex_vpc_network" "foo" {}
+
+resource "yandex_vpc_subnet" "foo" {
+  v4_cidr_blocks = ["10.2.0.0/16"]
+  zone       = "ru-central1-a"
+  network_id = "${yandex_vpc_network.foo.id}"
 }
 
 variable "YC_TOKEN" {
     type        = string
+}
+
+locals {
+  instance_count_map = {
+    stage = 1
+    prod = 2
+  }
 }
